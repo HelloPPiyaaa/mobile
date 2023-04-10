dace1109d1f15654867635b36ab3e95429c04d72
HelloPPiyaaa <127134814+HelloPPiyaaa@users.noreply.github.com>
2023-04-10 13:29:15 +0700

    seven

--- app/components/PlayListDetail.js
+++ app/components/PlayListDetail.js
@@ -1,30 +1,72 @@
 import React from 'react';
-import { AntDesign } from '@expo/vector-icons';
+import {
+  View,
+  StyleSheet,
+  Modal,
+  FlatList,
+  Text,
+  Dimensions,
+} from 'react-native';
+import { selectAudio } from '../misc/audioController';
 import color from '../misc/color';
-
-const PlayerButton = props => {
-  const { iconType, size = 40, iconColor = color.FONT, onPress } = props;
-  const getIconName = type => {
-    switch (type) {
-      case 'PLAY':
-        return 'pausecircle';
-      case 'PAUSE':
-        return 'playcircleo';
-      case 'NEXT':
-        return 'forward';
-      case 'PREV':
-        return 'banckward';
-    }
-  };
+import AudioListItem from './AudioListItem';
+const PlayListDetail = ({ visible, playList, onClose }) => {
+  const playAudio = (audio) => {
+    selectAudio(audio, )
+  }
+  
   return (
-    <AntDesign
-      {...props}
-      onPress={onPress}
-      name={getIconName(iconType)}
-      size={size}
-      color={iconColor}
-    />
+    <Modal
+      visible={visible}
+      animationType='slide'
+      transparent
+      onRequestClose={onClose}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>{playList.title}</Text>
+        <FlatList
+          contentContainerStyle={styles.listContainer}
+          data={playList.audios}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={{ marginBottom: 10 }}>
+              <AudioListItem title={item.filename} duration={item.duration} onAudioPress={()=> playAudio(item)} />
+            </View>
+          )}
+        />
+      </View>
+      <View style={[StyleSheet.absoluteFillObject, styles.modalBG]} />
+    </Modal>
   );
 };
 
-export default PlayerButton;
+const { width, height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 0,
+    alignSelf: 'center',
+    height: height - 150,
+    width: width - 15,
+    backgroundColor: '#fff',
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+  },
+  modalBG: {
+    backgroundColor: color.MODAL_BG,
+    zIndex: -1,
+  },
+  listContainer: {
+    padding: 20,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    paddingVertical: 5,
+    fontWeight: 'bold',
+    color: color.ACTIVE_BG,
+  },
+});
+
+export default PlayListDetail;
